261859f92b6f5a9a66e07059a8270270c21d9186
Gustavo Franke <franke.gustavo@gmail.com>
2020-09-23 23:09:27 +0100

    updates to ch07

--- src/main/scala/part2/ch07/ch07.scala
+++ src/main/scala/part2/ch07/ch07.scala
@@ -1,7 +1,5 @@
 package part2.ch07
 
-import java.util.concurrent.{Callable, ExecutorService}
-
 import java.util.concurrent._
 
 object Par {
@@ -37,6 +35,24 @@ object Par {
       val bf = b(es)
       UnitFuture(f(af.get, bf.get))
     }
+
+  /**
+    *
+    * EXERCISE 7.2
+    * Before continuing, try to come up with representations for Par
+    * that make it possible to implement the functions of our API.
+    * */
+  class ExecutorService {
+    def submit[A](a: Callable[A]): Future[A] = ???
+  }
+  trait Callable[A] { def call: A }
+  trait Future[A] {
+    def get: A
+    def get(timeout: Long, unit: TimeUnit): A
+    def cancel(evenIfRunning: Boolean): Boolean
+    def isDone: Boolean
+    def isCancelled: Boolean
+  }
 
   /**
     * EXERCISE 7.3
@@ -80,6 +96,23 @@ object Par {
       if (run(es)(cond).get) t(es) // Notice we are blocking on the result of `cond`.
       else f(es)
 
+  def parMap[A, B](ps: List[A])(f: A => B): Par[List[B]] = {
+    val fbs: List[Par[B]] = ps.map(asyncF(f))
+    sequence(fbs)
+  }
+
+  /**
+    * EXERCISE 7.5
+    * Hard: Write this function, called sequence. No additional primitives are required. Do not call run.
+    */
+  def sequence[A](ps: List[Par[A]]): Par[List[A]] = ???
+
+  /**
+    * EXERCISE 7.6
+    * Implement parFilter, which filters elements of a list in parallel.
+    */
+  def parFilter[A](as: List[A])(f: A => Boolean): Par[List[A]] = ???
+
   import language.implicitConversions
   /* Gives us infix syntax for `Par`. */
   implicit def toParOps[A](p: Par[A]): ParOps[A] = new ParOps(p)
@@ -122,4 +155,108 @@ class Sums {
     val (l,r) = ints.splitAt(ints.length/2)
     Par.map2(Par.fork(sum3(l)), Par.fork(sum3(r)))(_ + _)
   }
+}
+
+// 7.4 The algebra of an API
+object APIAlgebra {
+  import Par._
+
+  val lawOfMapping0: Boolean = map(unit(1))(_ + 1) == unit(2)
+  def lawOfMapping1[A](x: A, f: A => A): Boolean = map(unit(x))(f) == unit(f(x))
+
+  def equal[A](e: ExecutorService)(p: Par[A], p2: Par[A]): Boolean =
+    p(e).get == p2(e).get
+
+  //  map(unit(x))(f) == unit(f(x))   // Initial law.
+  //  map(unit(x))(id) == unit(id(x)) // Substitute identity function for f.
+  //  map(unit(x))(id) == unit(x) // Simplify.
+  //  map(y)(id) == y             // Substitute y for unit(x) on both sides.
+
+  /**
+    * EXERCISE 7.7
+    * Hard: Given map(y)(id) == y,
+    * it’s a free theorem that map(map(y)(g))(f) == map(y)(f compose g).
+    * (This is sometimes called map fusion,
+    * and it can be used as an optimization—rather than spawning a separate parallel
+    * computation to compute the second mapping,
+    * we can fold it into the first mapping.)
+    *
+    * Can you prove it?
+    * You may want to read the paper “Theorems for Free!” (http://mng.bz/Z9f1)
+    * to better under- stand the “trick” of free theorems.
+    * */
+
+  def lawOfForking[A](x:Par[A]): Boolean = fork(x) == x
+
+/**
+  * EXERCISE 7.8
+  * Hard: Take a look through the various static methods in Executors to
+  * get a feel for the different implementations of ExecutorService that exist.
+  * Then, before continuing,
+  * go back and revisit your implementation of fork and try to
+  * find a counterexample or convince yourself that the law holds for your implementation.
+  * There’s actually a rather subtle problem that will occur in most implementations of fork.
+  * When using an ExecutorService backed by a thread pool of bounded
+  * size (see Executors.newFixedThreadPool), it’s very easy to run into a deadlock.
+  *
+  * Suppose we have an ExecutorService backed by a thread pool where the maximum number of threads is 1.
+  * Try running the following example using our current implementation:
+  *
+  * */
+    val a = lazyUnit(42 + 1)
+    val S = Executors.newFixedThreadPool(1)
+//    println(Par.equal(S)(a, fork(a)))
+//  Most implementations of fork will result in this code deadlocking. Can you see why? Let’s look again at our implementation of fork:
+    def fork0[A](a: => Par[A]): Par[A] = es => es.submit(
+      new Callable[A] {
+        def call = a(es).get
+      })
+
+  /**
+    * EXERCISE 7.9
+    * Hard: Show that any fixed-size thread pool can be made to deadlock given this imple- mentation of fork.
+    */
+  def fork[A](fa: => Par[A]): Par[A] = es => fa(es)
+  def delay[A](fa: => Par[A]): Par[A] = es => fa(es)
+}
+
+object NonBlocking {
+  sealed trait Future[A] {
+    private[ch07] def apply(k: A => Unit): Unit
+  }
+  type Par[A] = ExecutorService => Future[A]
+
+  import java.util.concurrent.atomic.AtomicReference
+  def run[A](es: ExecutorService)(p: Par[A]): A = {
+    val ref = new AtomicReference[A]
+    val latch = new CountDownLatch(1)
+    p(es) { a => ref.set(a); latch.countDown }
+    latch.await
+    ref.get
+  }
+
+  def unit[A](a: A): Par[A] = es => new Future[A] {
+    def apply(cb: A => Unit): Unit = cb(a)
+  }
+
+  def fork[A](a: => Par[A]): Par[A] =
+    es => new Future[A] {
+      def apply(cb: A => Unit): Unit = eval(es)(a(es)(cb))
+    }
+  def eval(es: ExecutorService)(r: => Unit): Unit =
+    es.submit(new Callable[Unit] { def call = r })
+
+  /**
+    * To implement map2, we’ll use a non-blocking concurrency primitive called actors.
+    * An Actor is essentially a concurrent process that doesn’t constantly occupy a thread.
+    * Instead, it only occupies a thread when it receives a message.
+    * Importantly, although multiple threads may be concurrently sending messages to an actor,
+    * the actor pro- cesses only one message at a time,
+    * queueing other messages for subsequent process- ing.
+    * This makes them useful as a concurrency primitive when writing tricky code that
+    * must be accessed by multiple threads,
+    * and which would otherwise be prone to race conditions or deadlocks.
+    */
+  def map2[A,B,C](a: Par[A], b: Par[B])(f: (A,B) => C): Par[C] = ???
+
 }
