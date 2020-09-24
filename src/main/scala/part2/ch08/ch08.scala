abad821b03734870552ea32a331bb302e62b698f
Gustavo Franke <franke.gustavo@gmail.com>
2020-09-24 10:07:51 +0100

    updates to ch08

--- src/main/scala/part2/ch08/ch08.scala
+++ src/main/scala/part2/ch08/ch08.scala
@@ -134,16 +134,52 @@ object Asdf {
 //  case class Prop(run: TestCases => Result)
   case class Prop(run: (TestCases,RNG) => Result)
 
-  def forAll[A](as: Gen[A])(f: A => Boolean): Prop = Prop {
-    (n,rng) => randomStream(as)(rng).zip(Stream.from(0)).take(n).map {
-      case (a, i) => try {
-        if (f(a)) Passed else Falsified(a.toString, i)
-      } catch { case e: Exception => Falsified(buildMsg(a, e), i) }
-    }.find(_.isFalsified).getOrElse(Passed)
+  def forAll[A](as: Gen[A])(f: A => Boolean): Prop = Prop { (n, rng) =>
+    randomStream(as)(rng)
+      .zip(Stream.from(0))
+      .take(n)
+      .map { case (a, i) =>
+        try {
+          if (f(a)) Passed else Falsified(a.toString, i)
+        } catch {
+          case e: Exception => Falsified(buildMsg(a, e), i)
+        }
+      }.find(_.isFalsified).getOrElse(Passed)
   }
 
   def randomStream[A](g: Gen[A])(rng: RNG): Stream[A] = ??? // Stream.unfold(rng)(rng => Some(g.sample.run(rng)))
   def buildMsg[A](s: A, e: Exception): String =
     s"test case: $s\n" +
-      s"generated an exception: ${e.getMessage}\n" + s"stack trace:\n ${e.getStackTrace.mkString("\n")}"
+    s"generated an exception: ${e.getMessage}\n" +
+    s"stack trace:\n ${e.getStackTrace.mkString("\n")}"
+
+  /**
+    * EXERCISE 8.9
+    * Now that we have a representation of Prop,
+    * implement && and || for composing Prop values.
+    *
+    * Notice that in the case of failure we don’t know which property was responsi- ble, the left or the right.
+    * Can you devise a way of handling this,
+    * perhaps by allowing Prop values to be assigned a tag or label which
+    * gets displayed in the event of a failure?
+    */
+  def &&(p: Prop): Prop = ???
+  def ||(p: Prop): Prop = ???
+
+//  8.3 Test case minimization // page 134
+
+  /**
+    * EXERCISE 8.10
+    * Implement helper functions for converting Gen to SGen. You can add this as a method on Gen.
+    * def unsized: SGen[A]
+    *
+    * EXERCISE 8.11
+    * Not surprisingly,
+    * SGen at a minimum supports many of the same operations as Gen,
+    * and the implementations are rather mechanical. Define some convenience functions
+    *
+    * EXERCISE 8.12
+    * Implement a listOf combinator that doesn’t accept an explicit size. It should return an SGen instead of a Gen. The implementation should generate lists of the requested size.
+    * def listOf[A](g: Gen[A]): SGen[List[A]]
+    */
 }
